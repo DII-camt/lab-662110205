4f0a7c8ce78991cb2bbcb9c71da3c442996e6586
morjordor <151113683+662110205@users.noreply.github.com>
2024-07-15 15:15:12 +0700

    lab52

---
+++ lab5/lab5_2/test.js
@@ -0,0 +1,31 @@
+// const vehicles = ['mustang', 'f-150', 'expedition'];
+// const [a , ,b] = vehicles;
+// console.log(a); //mustang
+// console.log(b); //expedition
+
+// const vehicleOne = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     type: 'car',
+//     year: 2021,
+//     color: 'red'
+// }
+// console.log(vehicleOne.color); // red
+// console.log(vehicleOne["color"]);// red
+// console.log(vehicleOne["color "]);// undefined
+
+// const vehicleOne = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     type: 'car',
+//     year: 2021,
+//     color: 'red'
+// }
+// const { type, color, brand, model } = vehicleOne;
+// console.log(type);  // car
+// console.log(color); // red
+// console.log(brand); // Ford
+// console.log(model); // Mustang
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.pop());// Mango
